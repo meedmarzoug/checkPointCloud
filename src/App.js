b32c4c94f92ed8808e6508907b8557c56acacd9b
Mohamed Marzoug <mmarzoug8@gmail.com>
2023-01-30 23:14:18 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import Map, {NavigationControl} from 'react-map-gl';
+import Navbar from './components/navbar.js';
+import maplibregl from 'maplibre-gl';
+import 'maplibre-gl/dist/maplibre-gl.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Map mapLib={maplibregl} 
+        initialViewState={{
+          longitude: 36.885050,
+          latitude: 10.188556,
+          zoom: 14
+        }}
+        style={{width: "100%", height: " calc(100vh - 77px)"}}
+        mapStyle="https://api.maptiler.com/maps/streets/style.json?key=YOUR_MAPTILER_API_KEY_HERE"
+      >
+        <NavigationControl position="top-left" />
+      </Map>
+    </div>
+  );
+}
+
+export default App;
